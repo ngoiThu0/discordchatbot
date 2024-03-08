2a91a70150a6ac70d5876291f92ac46132144b5b
xq <kenguyen12397@gmail.com>
2024-03-09 02:56:23 +0700

    maybe last commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Your routes and other configurations go here
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Use the PORT environment variable if available, otherwise use 5000
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Run the Flask app on 0.0.0.0 (all available network interfaces)
+    app.run(host="0.0.0.0", port=port)
